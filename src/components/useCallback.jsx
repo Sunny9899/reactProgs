7e4c8613b4f75331237324c1e1c4c6ff1ff3dab6
Sunny9899 <suryanshpajiala@gmail.com>
2022-04-14 04:19:03 -0700

    CallBack

--- src/components/useCallback.jsx
+++ src/components/useCallback.jsx
@@ -1,3 +1,46 @@
-export const Callback=()=>{
-    
-}
+import { useState, useCallback } from "react";
+
+export const Callback = () => {
+  let [num, setNum] = useState(0);
+
+  let [col, setCol] = useState(false);
+
+  function costly(x) {
+    for (let i = 0; i < 1000000000; i++) {
+      x += i;
+    }
+    return x;
+  }
+
+  function getClick() {
+    setNum(num + 1);
+  }
+
+  const colorChange = {
+    color: col === false ? "red" : "green",
+  };
+
+  //costly(num);
+
+  useCallback(() => {
+    costly(num);
+  }, []); //This command when activated will instantaneously change color as well as update the count as compared to time it was taking to do same in before mentioned command
+
+  //useCallback saves the whole function actually unlike useMEmo
+
+  return (
+    <div>
+      <div>You clicked {num} times</div>
+      <button onClick={getClick}>Click</button>
+
+      <div style={colorChange}>Color Change</div>
+      <button
+        onClick={() => {
+          col === false ? setCol(true) : setCol(false);
+        }}
+      >
+        change
+      </button>
+    </div>
+  );
+};
--- src/components/useMemo.jsx
+++ src/components/useMemo.jsx
@@ -1,39 +1,44 @@
-import { useState, useMemo} from "react";
+import { useState, useMemo } from "react";
 
-export const Memo=()=>{
+export const Memo = () => {
+  let [num, setNum] = useState(0);
 
-    let [num,setNum]=useState(0);
-   
-    let [col,setCol]=useState(false);
+  let [col, setCol] = useState(false);
 
-    function costly(n){
-      for(var i=0;i<1000000000;i++){
-        n+=1;
-      }
-      return n;
+  function costly(n) {
+    for (var i = 0; i < 1000000000; i++) {
+      n += 1;
     }
+    return n;
+  }
 
-    const inc=()=>{
-      setNum((n)=>n+1)
-    }
+  const inc = () => {
+    setNum((n) => n + 1);
+  };
 
-    const colorChange={
-      color: col===false ? "red" : "green"
-    }
+  const colorChange = {
+    color: col === false ? "red" : "green",
+  };
 
-    costly(num) 
-    
-    //  useMemo(()=>{costly(num)},[num]); //This command when activated will instantaneously change color as compared to time it was taking to do same in before mentioned command
+  //costly(num)
 
-    return (
-        <div>
+  useMemo(() => {
+    costly(num);
+  }, [num]); //This command when activated will instantaneously change color as compared to time it was taking to do same in before mentioned command
 
-        <div>The num: {num}</div>
-        <button onClick={inc}>Increment</button>
+  return (
+    <div>
+      <div>The num: {num}</div>
+      <button onClick={inc}>Increment</button>
 
-        <div style={colorChange}>Color Change</div>
-        <button onClick={()=>{col===false ? setCol(true) : setCol(false)}}>change</button>
-        </div>
-    );
-    
-    }
+      <div style={colorChange}>Color Change</div>
+      <button
+        onClick={() => {
+          col === false ? setCol(true) : setCol(false);
+        }}
+      >
+        change
+      </button>
+    </div>
+  );
+};
