286b6e8058c5c61adb759acbf73c925a9a4f784d
Sunny9899 <suryanshpajiala@gmail.com>
2022-04-19 13:12:10 +0530

    CallBack Program Updated

--- src/components/useCallback.jsx
+++ src/components/useCallback.jsx
@@ -1,46 +1,26 @@
 import { useState, useCallback } from "react";
 
 export const Callback = () => {
-  let [num, setNum] = useState(0);
+  const[num,setNum]=useState(0);
 
-  let [col, setCol] = useState(false);
+  const [age,setAge]=useState(10);
+ 
+  const increaseNum=useCallback(()=>{
+    setNum(num+1);
+  },[num]);
 
-  function costly(x) {
-    for (let i = 0; i < 1000000000; i++) {
-      x += i;
-    }
-    return x;
-  }
-
-  function getClick() {
-    setNum(num + 1);
-  }
-
-  const colorChange = {
-    color: col === false ? "red" : "green",
-  };
-
-  //costly(num);
-
-  useCallback(() => {
-    costly(num);
-  }, []); //This command when activated will instantaneously change color as well as update the count as compared to time it was taking to do same in before mentioned command
-
-  //useCallback saves the whole function actually unlike useMEmo
+  const increaseAge=useCallback(()=>{
+    setAge(age+10);
+  },[age]);
 
   return (
     <div>
-      <div>You clicked {num} times</div>
-      <button onClick={getClick}>Click</button>
-
-      <div style={colorChange}>Color Change</div>
-      <button
-        onClick={() => {
-          col === false ? setCol(true) : setCol(false);
-        }}
-      >
-        change
-      </button>
+        <div>The Num is: {num}</div>
+        <button onClick={increaseNum}>Increase Num</button>
+        <br/>
+        <br/>
+        <div>The Age is: {age}</div>
+        <button onClick={increaseAge}>Increase Age</button>        
     </div>
   );
 };
